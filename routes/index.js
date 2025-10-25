5db308a878a54784bfcbbce957fe6da1afd9116a
binhcuong <binhcuongpy2014@gmail.com>
2025-10-25 17:00:41 +0700

    Merge code Linh

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 router.use('/chucvu', require('./chucvuRoutes'));
 router.use('/khuon-mat', require('./khuonmatRoutes'));
 router.use('/nhan-vien', require('./nhanvienRoutes'));
-app.use('/auth', require('./authRoutes'));
-app.use('/calamviec', require('./calamviecRoutes'));
-app.use('/phongban', require('./phongbanRoutes'));
+router.use('/auth', require('./authRoutes'));
+router.use('/calamviec', require('./calamviecRoutes'));
+router.use('/phongban', require('./phongbanRoutes'));
 
 module.exports = router;
