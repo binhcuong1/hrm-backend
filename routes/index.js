0c2f959ec6917989c46d4ef04973174c68b1f380
unknown <lyminhhai102@gmail.com>
2025-10-26 14:55:33 +0700

    sua routes index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const chucvuRoutes = require("./chucvuRoutes");
-const bangluongRoutes = require("./bangluongRoutes");
-
-router.use("/chucvu", chucvuRoutes);
-router.use("/bangluong", bangluongRoutes);
+router.use("/chucvu", require("./chucvuRoutes"));
+router.use("/bangluong", require("./bangluongRoutes"));
 
 module.exports = router;
